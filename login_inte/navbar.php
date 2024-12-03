b6861e8eddeb210b96dca9d7489630d87f0e7412
Pamelita123 <pame.ado3780@gmail.com>
2024-12-03 00:09:47 -0600

    barrav1

--- login_inte/navbar.php
+++ login_inte/navbar.php
@@ -14,16 +14,11 @@
                     <li>
                         <a class="nav-link" href="../ordenes/ordenes.php">Ordenes</a>
                     </li>
+                   
                     <li>
-                        <a class="nav-link" href="candidatos.html">Candidatos</a>
+                        <a class="nav-link" href="../clientes/clientes.php">Clientes</a>
                     </li>
-                    <li>
-                        <a class="nav-link" href="clientes.html">Clientes</a>
-                    </li>
-                    <li><a  class="perfili" href="perfil.html"><svg xmlns="http://www.w3.org/2000/svg" width="25" height="20" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
-                        <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0"/>
-                        <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1"/>
-                      </svg></a></li>
+                    
                 </ul>
             </div>
         </div>
